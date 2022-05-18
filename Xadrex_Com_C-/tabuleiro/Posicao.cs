c5f603ac024ca2c45dd62e59b594272a2b431ca8
lucas235 <lucass235@gmail.com>
2022-05-18 14:25:11 -0300

    criando a classe posicao

---
+++ Xadrex_Com_C-/tabuleiro/Posicao.cs
@@ -0,0 +1,16 @@
+﻿
+namespace tabuleiro {
+     class Posicao {
+        public int linha { get; set; }
+        public int coluna { get; set; }
+
+        public Posicao(int linha, int coluna) {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+
+        public override string? ToString() {
+            return $"Linha {this.linha}, Coluna {this.coluna}";
+        }
+    }
+}
