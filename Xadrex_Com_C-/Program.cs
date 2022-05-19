0f3bb40cffc9e9e4f839629bebea2ccde14bc4c3
lucas235 <lucass235@gmail.com>
2022-05-19 13:50:21 -0300

    tratando erros com excption custums

--- Xadrex_Com_C-/Program.cs
+++ Xadrex_Com_C-/Program.cs
@@ -5,18 +5,23 @@ using xadrez;
 namespace Xadrex_Com_C {
     class Program {
         static void Main(string[] args) {
-
+            try {
             Tabuleiro tab = new Tabuleiro(8, 8);
 
             tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(Cor.Preta, tab), new Posicao(2, 4));
+            tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 9));
+            tab.colocarPeca(new Rei(Cor.Preta, tab), new Posicao(0, 2));
 
 
 
             Tela.imprimirTabuleiro(tab);
 
             Console.ReadLine();
+
+            }
+            catch (TabuleiroException e) {
+                Console.WriteLine("Erro: " + e.Message);
+            }
         }
     }
 }
