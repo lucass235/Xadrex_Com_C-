e9276c61d3ecbbef9ba9df9c7858108377ec5755
lucas235 <lucass235@gmail.com>
2022-05-19 12:56:27 -0300

    começando a implementar o tabuleiro

--- Xadrex_Com_C-/Program.cs
+++ Xadrex_Com_C-/Program.cs
@@ -4,10 +4,7 @@ namespace Xadrex_Com_C {
     class Program {
         static void Main(string[] args) {
 
-            Posicao P;
-            P = new Posicao(3, 4);
-
-            Console.WriteLine("Posição " + P);
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
             Console.ReadLine();
         }
